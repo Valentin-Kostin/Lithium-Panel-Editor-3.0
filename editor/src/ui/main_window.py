019e3cdbb834046a40f3a06b5a722c4330afa14a
Valentin Kostin <frezer.2021@mail.ru>
2026-08-13 14:06:24 +0300

    003:q

--- editor/src/ui/main_window.py
+++ editor/src/ui/main_window.py
@@ -220,17 +220,27 @@ class MainWindow(QMainWindow):
     
     def _connect_signals(self):
         """Подключает сигналы."""
-        self.tree_view.selectionModel().currentChanged.connect(
-            self._on_tree_selection_changed
-        )
-        
-        self.property_editor.value_changed.connect(
-            self._on_property_changed
-        )
-        
-        self.operations_table.operation_selected.connect(
-            self._on_operation_selected
-        )
+    # Connect signals for both tabs
+    self.scx_tab.tree_view.selectionModel().currentChanged.connect(
+        self._on_tree_selection_changed
+    )
+    self.pgmx_tab.tree_view.selectionModel().currentChanged.connect(
+        self._on_tree_selection_changed
+    )
+    
+    self.scx_tab.property_editor.value_changed.connect(
+        self._on_property_changed
+    )
+    self.pgmx_tab.property_editor.value_changed.connect(
+        self._on_property_changed
+    )
+    
+    self.scx_tab.operations_table.selectionModel().currentChanged.connect(
+        self._on_operations_selection_changed
+    )
+    self.pgmx_tab.operations_table.selectionModel().currentChanged.connect(
+        self._on_operations_selection_changed
+    )
     
     def _load_mapping(self):
         """Загружает конфигурацию маппинга."""
